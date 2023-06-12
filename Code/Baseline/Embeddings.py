f8b5306cf52831acd56be518a06b30b9d8fb5744
A. Chakravarty <abhichak17@gmail.com>
2023-06-12 21:41:14 +0100

    fixing cursed embedding weight (1)

--- Code/Baseline/Embeddings.py
+++ Code/Baseline/Embeddings.py
@@ -7,9 +7,10 @@ class Skipgram_Util:
     def __init__(self, args, vocab):
         self.vocab = vocab
         self.embedding_layer = models.SkipGram(len(vocab), args.embedding_dim)
-        self.embedding_layer.embeddings.load_state_dict \
-        (torch.load(os.path.join(args.skipgram_file_path, 
-                                 f"skipgram_weights{args.prompt}_{args.embedding_dim}.pth"))["embeddings.weight"])
+        new_state_dict = {}
+        new_state_dict["weight"] = torch.load(os.path.join(args.skipgram_file_path, \
+                                 f"skipgram_weights{args.prompt}_{args.embedding_dim}.pth"))["embeddings.weight"]
+        self.embedding_layer.embeddings.load_state_dict (new_state_dict)
 
     def get_vecs_by_tokens(self, padded_tokens, lower_case_backup=True):
         input_indices = [self.vocab[token] for token in padded_tokens]
