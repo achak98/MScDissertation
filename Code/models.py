eb9a935a0a02ec58642e4d8ca8b09f8b37212b43
A. Chakravarty <abhichak17@gmail.com>
2023-06-13 00:59:14 +0100

    dropping out after concat

--- Code/models.py
+++ Code/models.py
@@ -41,22 +41,22 @@ class Baseline_Stride_One(nn.Module):
         x1 = self.pool1(x1)
         x1=x1.permute(0,2,1)
         _, h1 = self.gru1(x1) #x1 should be batch size, sequence length, input length
-        h1 = self.dropout1(h1)
         h1 = torch.cat((h1[0, :, :], h1[1, :, :]), dim=1)
+        h1 = self.dropout1(h1)
 
         x2 = self.conv2(x)
         x2 = self.pool2(x2)
         x2=x2.permute(0,2,1)
         _, h2 = self.gru2(x2)
-        h2 = self.dropout1(h2)
         h2 = torch.cat((h2[0, :, :], h2[1, :, :]), dim=1)
+        h2 = self.dropout1(h2)
 
         x3 = self.conv3(x)
         x3 = self.pool3(x3)
         x3=x3.permute(0,2,1)
         _, h3 = self.gru3(x3)
-        h3 = self.dropout1(h3)
         h3 = torch.cat((h3[0, :, :], h3[1, :, :]), dim=1)
+        h3 = self.dropout1(h3)
 
         h = torch.cat((h1, h2, h3), dim=1)
         h = self.fc(h)
@@ -92,22 +92,22 @@ class Baseline(nn.Module):
         x1 = self.pool1(x1)
         x1=x1.permute(0,2,1)
         _, h1 = self.gru1(x1) #x1 should be batch size, sequence length, input length
-        h1 = self.dropout1(h1)
         h1 = torch.cat((h1[0, :, :], h1[1, :, :]), dim=1)
+        h1 = self.dropout1(h1)
 
         x2 = self.conv2(x)
         x2 = self.pool2(x2)
         x2=x2.permute(0,2,1)
         _, h2 = self.gru2(x2)
-        h2 = self.dropout1(h2)
         h2 = torch.cat((h2[0, :, :], h2[1, :, :]), dim=1)
+        h2 = self.dropout1(h2)
 
         x3 = self.conv3(x)
         x3 = self.pool3(x3)
         x3=x3.permute(0,2,1)
         _, h3 = self.gru3(x3)
-        h3 = self.dropout1(h3)
         h3 = torch.cat((h3[0, :, :], h3[1, :, :]), dim=1)
+        h3 = self.dropout1(h3)
 
         h = torch.cat((h1, h2, h3), dim=1)
         h = self.fc(h)
