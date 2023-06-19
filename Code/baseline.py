1a137e537f51eccada7803698b1e9351b568a31d
A. Chakravarty <abhichak17@gmail.com>
2023-06-20 00:09:10 +0100

    he doesnt know shit stfu

--- Code/baseline.py
+++ Code/baseline.py
@@ -11,54 +11,57 @@ def main():
     args = argsparser.parse_args()
     for embedding_type in embedding_types:
         for stride1 in range(1, args.cnn_window_size_small+1):
-            for stride2 in range (1, args.cnn_window_size_small+1):
-                for stride3 in range (1, args.cnn_window_size_medium+1):
-                    for stride4 in range (1, args.cnn_window_size_medium+1):
-                        for stride5 in range (1, args.cnn_window_size_large+1):
-                            for stride6 in range (1, args.cnn_window_size_large+1):
-                                qwk_score = []
-                                seeds = []
-                                random.seed(42)
-                                # Write data to the file
-                                data = f"For embedding type: {embedding_type} stride1: {stride1} stride2:{stride2} stride3: {stride3} stride4: {stride4} stride5: {stride5} stride6: {stride6} \n"
+            for ks1 in range(1, args.cnn_window_size_small+1):
+                for stride2 in range (1, args.cnn_window_size_small+1):
+                    for stride3 in range (1, args.cnn_window_size_medium+1):
+                        for ks2 in range (1, args.cnn_window_size_medium+1):
+                            for stride4 in range (1, args.cnn_window_size_medium+1):
+                                for stride5 in range (1, args.cnn_window_size_large+1):
+                                    for ks3 in range (1, args.cnn_window_size_large+1):
+                                        for stride6 in range (1, args.cnn_window_size_large+1):
+                                            qwk_score = []
+                                            seeds = []
+                                            random.seed(42)
+                                            # Write data to the file
+                                            data = f"For embedding type: {embedding_type} stride1: {stride1} ks1: {ks1} stride2:{stride2} stride3: {stride3} ks2: {ks2} stride4: {stride4} stride5: {stride5} ks3: {ks3} stride6: {stride6} \n"
 
-                                for i in range (10):
-                                    seed = random.randint(1, 1000)
+                                            for i in range (10):
+                                                seed = random.randint(1, 1000)
 
-                                    # Set the random seed for Python's random module
-                                    random.seed(seed)
+                                                # Set the random seed for Python's random module
+                                                random.seed(seed)
 
-                                    # Set the random seed for NumPy
-                                    np.random.seed(seed)
+                                                # Set the random seed for NumPy
+                                                np.random.seed(seed)
 
-                                    # Set the random seed for PyTorch
-                                    torch.manual_seed(seed)
+                                                # Set the random seed for PyTorch
+                                                torch.manual_seed(seed)
 
-                                    # Set the random seed for CUDA operations (if using GPUs)
-                                    torch.cuda.manual_seed(seed)
-                                    torch.cuda.manual_seed_all(seed)
+                                                # Set the random seed for CUDA operations (if using GPUs)
+                                                torch.cuda.manual_seed(seed)
+                                                torch.cuda.manual_seed_all(seed)
 
-                                    # Set the deterministic behavior for cudNN (if using GPUs)
-                                    torch.backends.cudnn.deterministic = True
-                                    torch.backends.cudnn.benchmark = False
+                                                # Set the deterministic behavior for cudNN (if using GPUs)
+                                                torch.backends.cudnn.deterministic = True
+                                                torch.backends.cudnn.benchmark = False
 
-                                    # Parse the arguments
-                                    qwk_score.append(baseline(args, embedding_type, stride1, stride2, stride3, stride4, stride5, stride6))
-                                    seeds.append(seed)
+                                                # Parse the arguments
+                                                qwk_score.append(baseline(args, embedding_type, stride1, stride2, stride3, stride4, stride5, stride6, ks1, ks2, ks3))
+                                                seeds.append(seed)
 
-                                file = open(file_path, "a")
+                                            file = open(file_path, "a")
 
-                                # Loop through the zipped pairs
-                                for seed, score in zip(seeds, qwk_score):
-                                    data += f"For seed {seed}, score was {score} \n" 
-                                data += f"Average QWK score: {np.average(qwk_score)} \n"
-                                file.write(data)
+                                            # Loop through the zipped pairs
+                                            for seed, score in zip(seeds, qwk_score):
+                                                data += f"For seed {seed}, score was {score} \n" 
+                                            data += f"Average QWK score: {np.average(qwk_score)} \n"
+                                            file.write(data)
 
-                                # Close the file
-                                file.close()
-    
+                                            # Close the file
+                                            file.close()
+        
 
-def baseline(args, embedding_type, stride1, stride2, stride3, stride4, stride5, stride6):
+def baseline(args, embedding_type, stride1, stride2, stride3, stride4, stride5, stride6, ks1, ks2, ks3):
     print("!!========================CREATING DATA LOADERS========================!!")
     train_dataloader, val_dataloader, test_dataloader, _ = dataloaders \
     .create_data_loaders(args, embedding_type = embedding_type)
@@ -66,7 +69,7 @@ def baseline(args, embedding_type, stride1, stride2, stride3, stride4, stride5, 
     #print("VOCAB_SIZE: ", vocab_size)
     print("!!========================INSTANTIATING MODEL========================!!")
     # Instantiate your model
-    model = models.Baseline(args, stride1, stride2, stride3, stride4, stride5, stride6)
+    model = models.Baseline(args, stride1, stride2, stride3, stride4, stride5, stride6, ks1, ks2, ks3)
 
     # Define your loss function and optimizer
     print("!!========================TRAINING MODEL========================!!")
