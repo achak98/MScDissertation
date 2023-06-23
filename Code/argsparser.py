d418b875f38e80e4797def18196890b5e0626716
A. Chakravarty <abhichak17@gmail.com>
2023-06-23 20:44:00 +0100

    tres mal

--- Code/argsparser.py
+++ Code/argsparser.py
@@ -6,7 +6,7 @@ def parse_args():
     # Add arguments
     parser.add_argument('--dataDir', type=str, help='Data Directory')
     parser.add_argument('--skipgram_file_path', type=str, help='Skipgram File Directory')
-    parser.add_argument('--numOfWorkers', type=str, help='Number of Workers for Dataloaders')
+    parser.add_argument('--numOfWorkers', type=int, help='Number of Workers for Dataloaders')
     parser.add_argument('--embedding_dim', type=int, default = 300, help='Embedding Dimension')
     parser.add_argument('--num_epochs', type=int, default = 40, help='No. of Epochs')
     parser.add_argument('--batch_size', type= int, default = 128, help = 'Batch Size')
@@ -20,5 +20,5 @@ def parse_args():
     parser.add_argument('--prompt', type= int, default = 1, help = 'Prompt to train and test for')
     parser.add_argument('--max_length', type= int, default = 768, help = 'Padding sequence length')
     parser.add_argument('--dropout', type= float, default = 0.4, help = 'Dropout Rate')
-    parser.add_argument('--log_interval', type= int, default = 0.4, help = 'Loggin Interval in Epochs')
+    parser.add_argument('--log_interval', type= float, default = 0.4, help = 'Loggin Interval in Epochs')
     return parser.parse_args()
