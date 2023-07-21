eb37109358fdd417fdd11bae74fc95b2d8ca0d4e
A. Chakravarty <abhichak17@gmail.com>
2023-07-21 13:20:13 +0100

    yeet

--- Code/edudetection.py
+++ Code/edudetection.py
@@ -339,7 +339,6 @@ def main():
             for step, (embeddings,labels) in train_loader_tqdm:
                 inputs = embeddings.to(torch.float) #.to(device)
                 labels = labels.to(device)
-                labels = labels.to(torch.float)
                 print(f"type of inputs tensor: {inputs.dtype}, and type of labels tensor: {labels.dtype}") 
 
                 optimizer.zero_grad()  # Zero the gradients
