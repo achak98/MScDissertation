50d51672dcf4464bffc871960e18920b866055f5
A. Chakravarty <abhichak17@gmail.com>
2023-07-21 03:05:04 +0100

    yeet

--- Code/edudetection.py
+++ Code/edudetection.py
@@ -216,7 +216,7 @@ class EDUPredictor(nn.Module):
         self.tokenizer = AutoTokenizer.from_pretrained(self.transformer_architecture, max_length=self.config.max_position_embeddings, padding="max_length", return_attention_mask=True)
 
         # Define BiLSTM 1
-        self.lstm1 = nn.LSTM(max_length, hidden_dim, num_layers=2, bidirectional=True)
+        self.lstm1 = nn.LSTM(hidden_dim, hidden_dim, num_layers=2, bidirectional=True)
 
         # Define self-attention
         self.self_attention = SelfAttention(hidden_dim)
