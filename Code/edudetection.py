51f2582379ac2e1b87806305d1dde5f0b42fe477
A. Chakravarty <abhichak17@gmail.com>
2023-07-21 06:00:07 +0100

    yeet

--- Code/edudetection.py
+++ Code/edudetection.py
@@ -157,7 +157,7 @@ def preprocess_RST_Discourse_dataset(path_data, tag2idx, args, model):
             text = text.split('\n')
             text = "[CLS]" + ' '.join(text).replace(". ", ".[SEP] ")
             text = text.replace("'", " \' ").replace("\"", " \" ").replace("-", " - ").replace(",", " , ").replace(".", " , ")
-            edus = [edu.replace("'", " \' ").replace("\"", " \" ").replace("-", " - ").replace(",", " , ").replace(".", " , ") for edu in edus]
+            edus = [edu.replace("'", " \' ").replace("\"", " \" ").replace("-", " - ").replace(",", " , ").replace(". ", ".[SEP] ").replace(".", " , ") for edu in edus]
             edus = [seq.strip() for seq in edus]
 
             #words = re.findall(args.regex_pattern, ' '.join(text))
