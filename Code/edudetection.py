4dff665639e4cb4050f7cf4b64776ecc91b14aad
A. Chakravarty <abhichak17@gmail.com>
2023-07-21 13:13:22 +0100

    yeet

--- Code/edudetection.py
+++ Code/edudetection.py
@@ -338,8 +338,10 @@ def main():
             train_loader_tqdm = tqdm(enumerate(train_loader), total=len(train_loader), desc='Batches')
             for step, (embeddings,labels) in train_loader_tqdm:
                 inputs = embeddings.to(torch.float64) #.to(device)
-                labels = labels.to(device).to(torch.float64)
-                model = model.to(device).double()
+                labels = labels.to(device)
+                labels = labels.to(torch.float64)
+                print(f"type of inputs tensor: {inputs.dtype}, and type of labels tensor: {labels.dtype}") 
+                
                 optimizer.zero_grad()  # Zero the gradients
 
                 # Forward propagation
