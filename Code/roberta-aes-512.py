d9bf64071ae81e4de63d1d37ed2c566537164869
A. Chakravarty <abhichak17@gmail.com>
2023-07-25 18:44:08 +0100

    yeet

--- Code/roberta-aes-512.py
+++ Code/roberta-aes-512.py
@@ -8,7 +8,7 @@ from torch.utils.data import DataLoader, TensorDataset
 from transformers import RobertaTokenizer, RobertaModel
 from matplotlib import pyplot as plt
 from tqdm.auto import tqdm
-
+import torch.nn as nn
 
 # set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
