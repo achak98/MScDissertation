71c79374e6d3ccf7742ea2a11ae5fdc1612ce9ea
A. Chakravarty <abhichak17@gmail.com>
2023-07-26 20:25:23 +0100

    yeet

--- Code/roberta-aes-512.py
+++ Code/roberta-aes-512.py
@@ -79,7 +79,7 @@ def mean_encoding(essay_list, model, tokenizer):
   embeddings = []
   for essay in tqdm(essay_list):
     encoded_input = tokenizer(essay, padding="max_length", truncation=True, max_length=512, return_tensors='pt', return_attention_mask=True).to(device)
-    print(encoded_input.size())
+    print(type(encoded_input))
     with torch.no_grad():
       model_output = model(**encoded_input)
     tokens_embeddings = np.matrix(model_output[0].squeeze().cpu())
