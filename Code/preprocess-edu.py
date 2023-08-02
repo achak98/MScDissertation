d16ff5fe63b5018543c01a8d559eab549b7d1f92
A. Chakravarty <abhichak17@gmail.com>
2023-08-02 16:30:28 +0100

    yeet

--- Code/preprocess-edu.py
+++ Code/preprocess-edu.py
@@ -1,66 +1,33 @@
-import os
 import pandas as pd
+import os
+from tqdm import tqdm
+data_dir = "./../Data/ASAP-AES/"
+# Original kaggle training set
+kaggle_dataset = pd.read_csv(
+    os.path.join(data_dir,"training_set_rel3.tsv"), sep="\t", encoding="ISO-8859-1"
+)
+# Smaller training set used for this project
+dataset = pd.DataFrame(
+    {
+        "essay_id": kaggle_dataset["essay_id"],
+        "essay_set": kaggle_dataset["essay_set"],
+        "essay": kaggle_dataset["essay"],
+        "rater1": kaggle_dataset["rater1_domain1"],
+        "rater2": kaggle_dataset["rater2_domain1"],
+        "score": kaggle_dataset["domain1_score"],
+    }
+)
+def check_and_create_directory(directory_path):
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+        print(f"Directory '{directory_path}' created.")
+    else:
+        print(f"Directory '{directory_path}' already exists.")
 
-def generate_bieo_labels(base_text, edus):
-    labels = []
-    words = base_text.split()
-    edu_index = 0
-    edu_words = edus[edu_index].split()
+check_and_create_directory(os.path.join(data_dir,"edu_in"))
 
-    for word in words:
-        if word == edu_words[0]:
-            if len(edu_words) == 1:
-                labels.append("B")
-                edu_index += 1
-                if edu_index < len(edus):
-                    edu_words = edus[edu_index].split()
-            else:
-                labels.append("B")
-                edu_words = edu_words[1:]
-        elif word in edu_words and edu_words[-1] != word:
-            labels.append("I")
-        elif word == edu_words[-1]:
-            labels.append("E")
-            edu_index += 1
-            if edu_index < len(edus):
-                edu_words = edus[edu_index].split()
-        else:
-            labels.append("O")
-
-    return labels
-
-def process_files(text_file, edu_file):
-    with open(text_file, 'r') as f:
-        base_text = f.read().strip()
-
-    with open(edu_file, 'r') as f:
-        edus = f.read().strip().split('\n')
-
-    labels = generate_bieo_labels(base_text, edus)
-    return base_text, labels
-
-# Directory paths for text and EDU files
-text_dir = 'path/to/text/files'
-edu_dir = 'path/to/edu/files'
-
-# Output CSV file path
-output_csv = 'output.csv'
-
-# Process files in the directory
-data = []
-for filename in os.listdir(text_dir):
-    if filename.endswith('.txt'):
-        text_file = os.path.join(text_dir, filename)
-        edu_file = os.path.join(edu_dir, filename.replace('.txt', '.edu'))
-        base_text, labels = process_files(text_file, edu_file)
-        data.append([base_text, ' '.join(labels)])
-
-# Create a DataFrame and save as CSV
-df = pd.DataFrame(data, columns=['Text', 'Labels'])
-df.to_csv(output_csv, index=False)
-
-# Read the CSV file as a DataFrame
-df = pd.read_csv(output_csv)
-
-# Display the DataFrame
-print(df)
+for (essay,essay_id) in tqdm(zip(dataset["essay"], dataset["essay_id"])):
+    modified_essay = essay.replace(". ", ".\n")
+    output_file_name = os.path.join(data_dir,"edu_in",f"{essay_id}.out")
+    with open(output_file_name, "w") as file:
+        file.write(modified_essay)
