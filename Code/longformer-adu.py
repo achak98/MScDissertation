1b495bff635c4b2ba188232b68484d5eff086a5d
A. Chakravarty <abhichak17@gmail.com>
2023-08-10 18:17:21 +0100

    yeet

--- Code/longformer-adu.py
+++ Code/longformer-adu.py
@@ -160,7 +160,7 @@ def get_loader(df, id2emb, essay_embeddings, shuffle=True):
 
   # dataset and dataloader
   data = TensorDataset(torch.from_numpy(embeddings).float(), torch.from_numpy(np.array(df['scaled_score'])).float())
-  loader = DataLoader(data, batch_size=64, shuffle=shuffle, num_workers=2)
+  loader = DataLoader(data, batch_size=64, shuffle=shuffle, num_workers=0)
 
   return loader
 
